aed55534ea4540813ea595f092c33d11223aa495
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-08-28 10:07:06 +0200

    wip

--- src/components/InputsComponent.vue
+++ src/components/InputsComponent.vue
@@ -1,4 +1,28 @@
 <script setup lang="ts">
+import { onMounted, watch } from 'vue'
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+onMounted(() => {
+  const namespace = String(route.params.namespace)
+  const slug = String(route.params.slug)
+  console.info(namespace)
+  console.info(slug)
+  // if (namespace && packageName) initInputs(namespace, packageName)
+})
+
+// 2️⃣ React if the URL changes or if the param shows up later
+watch(
+  () => route.query.package,
+  (pkg) => {
+    const namespace = String(route.params.namespace)
+    const slug = String(route.params.slug)
+    console.info(namespace)
+    console.info(slug)
+    // if (typeof pkg === 'string' && pkg.trim().length) initInputs(namespace, packageName)
+  }
+)
 
 </script>
 
